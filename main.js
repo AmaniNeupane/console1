1d2517fdb1136e986f2804476c347189863ee280
AmaniNeupane <96517638+AmaniNeupane@users.noreply.github.com>
2023-09-02 10:45:32 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "Ranbir";
+var last_name = "Nat";
+var a = "10";
+var b = "5";
+var add = a + b ;
+console.log(add)
+first_name.concat(last_name);;
+console.log("My console project!");
